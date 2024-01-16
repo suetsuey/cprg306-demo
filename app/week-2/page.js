6b2d62be02ab69306f9012a60104435556785f72
suetsuey <140632841+suetsuey@users.noreply.github.com>
2024-01-16 12:59:07 -0700

    week2 page

---
+++ app/week-2/page.js
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main>
+      <h1 className="text-4xl">Week 2 Demos</h1>
+      <p>This ia a paragraph</p>
+      <p>This ia an other paragraph</p>
+    </main>
+  );
+}
